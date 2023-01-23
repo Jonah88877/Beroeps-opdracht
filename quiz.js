84a9e14ef00e2093bdeee10c76dc14684f995b89
Jonah88877 <121024288+Jonah88877@users.noreply.github.com>
2023-01-23 12:40:11 +0100

    Mijn quiz over gta toegevoegd

---
+++ quiz.js
@@ -0,0 +1,10 @@
+function fout(){
+    document.getElementById('q1').style.border = '4px solid red'
+}
+
+
+function goed(){
+    document.getElementById('q1').style.border = '4px solid green'
+    
+}
+
