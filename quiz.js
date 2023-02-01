9df6e7d1e2ffa90caf4fea7221ca657328f9275e
Jonah88877 <121024288+Jonah88877@users.noreply.github.com>
2023-02-01 14:39:25 +0100

    Updated codes gedeeld

--- quiz.js
+++ quiz.js
@@ -1,10 +1,35 @@
-function fout(){
-    document.getElementById('q1').style.border = '4px solid red'
+function myFunction() {
+    let goedeAntwoorden = ["c", "c", "c", "a", "a", "a", "b", "d", "d", "c"];
+    let antwoorden = [];
+    let count = 0;
+
+    for (let i = 1; i <= 10; i++) {
+        let gebruikerAntwoord = document.querySelector('input[name="question' + i + '"]:checked');
+
+        if (gebruikerAntwoord) {
+            antwoorden.push(gebruikerAntwoord.value);
+        }
+    }
+
+    for (let i = 0; i < 10; i++) {
+        if (antwoorden[i] == goedeAntwoorden[i]) {
+            count++;
+        }
+    }
+    let totaal = "Je hebt " + count + " van de 15 vragen goed.";
+    alert(totaal);
+
 }
 
 
-function goed(){
-    document.getElementById('q1').style.border = '4px solid green'
-    
-}
+function showNextQuestion(questionNumber) {
+    let currentQuestion = document.getElementById("q" + questionNumber);
+    currentQuestion.style.display = "block";
 
+    for (let i = 1; i <= 15; i++) {
+        if (i !== questionNumber) {
+            let otherQuestion = document.getElementById("q" + i);
+            otherQuestion.style.display = "none";
+        }
+    }
+}
